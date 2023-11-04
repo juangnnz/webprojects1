18b78ba7469b868f7081979e3b08ad2be3f5c54c
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-04 18:10:11 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,25 @@
   <div>
     <header>
       <h1>Welcome to Battle Arena!</h1>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Player Management</router-link>
+            <ul class="submenu">
+              <li><router-link to="/player-info">Player Information</router-link></li>
+              <li><router-link to="/register">Register User</router-link></li>
+              <li><router-link to="/join">Join User</router-link></li>
+              <li><router-link to="/delete">Delete User</router-link></li>
+            </ul>
+          </li>
+          <li><router-link to="/attacks">Management of Attacks</router-link></li>
+          <li><router-link to="/players-list">List Players</router-link></li>
+          <li><router-link to="/game-history">Players Game History</router-link></li>
+          <li><router-link to="/arenas">Arenas Management</router-link></li>
+          <li><router-link to="/store">Store</router-link></li>
+        </ul>
+      </nav>
     </header>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Player Management</router-link>
-          <ul class="submenu">
-            <li><router-link to="/player-info">Player Information</router-link></li>
-            <li><router-link to="/register">Register User</router-link></li>
-            <li><router-link to="/join">Join User</router-link></li>
-            <li><router-link to="/delete">Delete User</router-link></li>
-          </ul>
-        </li>
-        <li><router-link to="/attacks">Management of Attacks</router-link></li>
-        <li><router-link to="/players-list">List Players</router-link></li>
-        <li><router-link to="/game-history">Players Game History</router-link></li>
-        <li><router-link to="/arenas">Arenas Management</router-link></li>
-        <li><router-link to="/store">Store</router-link></li>
-      </ul>
-    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -94,5 +94,16 @@
     background-color: #fff;
     padding: 20px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  header nav {
+    background-color: #354a5e;
+  }
+  
+  header nav ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
   }
 </style>

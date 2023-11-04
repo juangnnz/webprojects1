44dead3916fc7ef59ee5e76765ef8118062d54b7
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-04 19:27:50 +0100

    making the menu more responsive

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Welcome to Battle Arena!</h1>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Player Management</router-link>
+            <ul class="submenu">
+              <li><router-link to="/player-info">Player Information</router-link></li>
+              <li><router-link to="/register">Register User</router-link></li>
+              <li><router-link to="/join">Join User</router-link></li>
+              <li><router-link to="/delete">Delete User</router-link></li>
+            </ul>
+          </li>
+          <li><router-link to="/attacks">Management of Attacks</router-link></li>
+          <li><router-link to="/players-list">List Players</router-link></li>
+          <li><router-link to="/game-history">Players Game History</router-link></li>
+          <li><router-link to="/arenas">Arenas Management</router-link></li>
+          <li><router-link to="/store">Store</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <router-view></router-view> 
+  </div>
+</template>
+
 <style>
   body {
     font-family: Arial, sans-serif;

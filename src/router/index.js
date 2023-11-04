4df3c45c09635874775ce2dd0d5f694276c98c2d
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-04 17:36:25 +0100

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Registration from './components/Registration.vue';
-import PlayerInfo from './components/PlayerInfo.vue';
+import Registration from './components/registration.vue';
+import PlayerInfo from './components/playerInfo.vue';
 
 const routes = [
   {
     path: '/register',
-    name: 'Registration', 
+    name: 'registration', 
     component: registration, 
   },
   {

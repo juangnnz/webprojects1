dda73f4a8cfa042f8a720aa1cdb208b1480dd4c2
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-06 13:59:23 +0100

    adding all the componets

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import registration from '../components/registration.vue';
 import playerInfo from '../components/playerInfo.vue';
+import join from '../components/join.vue';
+import deletePlayer from '../components/deletePlayer.vue';
+import attacks from '../components/attacks.vue';
+import playerLists from '../components/playerLists.vue';
+import gameHistory from '../components/gameHistory.vue';
+import arenas from '../components/arenas.vue';
+import store from '../components/store.vue';
 
 const routes = [
-    
   {
     path: '/register',
-    name: 'registration', 
-    component: registration, 
+    name: 'registration',
+    component: registration,
   },
   {
     path: '/player-info',
-    name: 'PlayerInfo', 
-    component: playerInfo, 
+    name: 'PlayerInfo',
+    component: playerInfo,
+  },
+  {
+    path: '/join',
+    name: 'Join',
+    component: join,
+  },
+  {
+    path: '/delete',
+    name: 'Delete',
+    component: deletePlayer,
+  },
+  {
+    path: '/attacks',
+    name: 'Attacks',
+    component: attacks,
+  },
+  {
+    path: '/player-lists',
+    name: 'PlayerLists',
+    component: playerLists,
+  },
+  {
+    path: '/game-history',
+    name: 'GameHistory',
+    component: gameHistory,
+  },
+  {
+    path: '/arenas',
+    name: 'Arenas',
+    component: arenas,
+  },
+  {
+    path: '/store',
+    name: 'Store',
+    component: store,
   },
 ];
 

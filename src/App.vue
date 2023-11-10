26bb420d55cc0c5f93f0227de33a37a534374e77
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-10 12:36:42 +0100

    adding some logic the logo

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,45 @@
 <template>
   <div class="login-form">
-     <h2>Log In</h2>
-     <input type="text" v-model="userId" placeholder="Enter ID" />
-     <input type="password" v-model="password" placeholder="Enter password" />
-     <button @click="login">Login</button>
- 
-     <h2>Sign Up</h2>
-     <button @click="signup">Sign Up</button>
+    <h2>Log In</h2>
+    <logo></logo> 
+    <input type="text" v-model="userId" placeholder="Enter ID" />
+    <input type="password" v-model="password" placeholder="Enter password" />
+    <button @click="login">Login</button>
+
+    <h2>Sign Up</h2>
+    <button @click="signup">Sign Up</button>
   </div>
- </template>
- 
- <script>
- export default {
+</template>
+
+<script>
+
+import Logo from '@/components/logo.vue'; // Import the Logo component
+
+export default {
+  components: {
+    Logo, // Register the Logo component
+  },
   data() {
-     return {
-       userId: '',
-       password: '',
-     };
+    return {
+      userId: '',
+      password: '',
+    };
   },
   methods: {
-     login() {
-       // 
-       //
-     },
-     signup() {
-       // 
-       // 
-     },
+    login() {
+      // Your login logic here
+    },
+    signup() {
+      // Your signup logic here
+    },
   },
- };
- </script>
- 
- <style scoped>
- .login-form {
+};
+</script>
+
+<style scoped>
+.login-form {
   display: flex;
   flex-direction: column;
   align-items: center;
- }
- </style>
+}
+</style>

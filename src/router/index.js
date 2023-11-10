1cc959e747b255f89332cc811ff421de3f9a0be8
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-10 17:22:23 +0100

    updating routing

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,10 @@ import store from '../components/store.vue';
 import createGame from '../components/createGame.vue';
 import availableGames from '../components/availableGames.vue';
 import gameFinder from '../components/gameFinder.vue';
-
+import PlayerHistory from '../components/PlayerHistory.vue';
+import arena from '../components/arena.vue';
+import BuyAttacks from '../components/BuyAttacks.vue';
+import CreateSellAttacks from '../components/CreateSellAttacks.vue';
 
 
 const routes = [
@@ -25,15 +28,31 @@ const routes = [
     component: registration,
   },
   {
+    path: '/player-history',
+    name: 'PlayerHistory',
+    component: PlayerHistory,
+  },
+  {
+    path: '/arena',
+    name: 'arena',
+    component: arena,
+  },
+  {
+    path: '/buy-attacks',
+    name: 'BuyAttacks',
+    component: BuyAttacks,
+  },
+  {
+    path: '/create-sell-attacks',
+    name: 'CreateSellAttacks',
+    component: CreateSellAttacks,
+  },
+  {
     path: '/player-info',
     name: 'PlayerInfo',
     component: playerInfo,
   },
-  {
-    path: '/delete',
-    name: 'Delete',
-    component: deletePlayer,
-  },
+  
   {
     path: '/attacks',
     name: 'Attacks',

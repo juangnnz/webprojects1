519305d0a5e98c5e325a6f3db3bd9bed0635b488
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-10 16:50:36 +0100

    cambio cosas del search games en funcion de mockup

--- src/components/gameFinder.vue
+++ src/components/gameFinder.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="game-finder">
-      <h2>List of All Games</h2>
-      
-      <div class="filters">
-        <label>Status:</label>
-        <select v-model="filterStatus">
+  <div class="game-finder">
+    <h2>List of All Games</h2>
+
+    <div class="filters">
+      <select v-model="filterStatus">
           <option value="all">All</option>
           <option value="finished">Finished</option>
           <option value="available">Available</option>
@@ -16,40 +15,56 @@
   
         <label>Search:</label>
         <input type="text" v-model="searchQuery" @input="searchGames" />
-      </div>
-  
-      <ul class="game-list">
-        <li v-for="game in filteredGames" :key="game.id" class="game-item">
-          <span class="game-name">{{ game.name }}</span>
-          <button class="view-details-button" @click="viewGameDetails(game.id)">View Details</button>
-        </li>
-      </ul>
-  
-      <div v-if="selectedGame" class="game-details">
-        <h3>Game Details</h3>
-        <p><strong>Name:</strong> {{ selectedGame.name }}</p>
-        <p><strong>Status:</strong> {{ selectedGame.status }}</p>
-        <!-- aqui toda la info de los logs y game details del juego  -->
-      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        games: [], // aqui cogeremos todo de la API
+
+    <ul class="game-list">
+      <li v-for="game in filteredGames" :key="game.id" class="game-item">
+        <span class="game-name">{{ game.name }}</span>
+        <button class="view-details-button" @click="viewGameDetails(game.id)">View Details</button>
+      </li>
+    </ul>
+
+    <div v-if="selectedGame" class="game-details">
+      <h3>Game Details</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Game ID</th>
+            <th>Size</th>
+            <th>HP_max</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ selectedGame.id }}</td>
+            <td>{{ selectedGame.size }}</td>
+            <td>{{ selectedGame.HP_max }}</td>
+            <td><button @click="viewRecord(selectedGame.id)">View Record</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      games: [], // aqui cogeremos todo de la API
         filterStatus: 'all',
         startDate: '',
         endDate: '',
         searchQuery: '',
         selectedGame: null,
       };
-    },
-    computed: {
-      filteredGames() {
-        // filtros aqui
-        return this.games.filter((game) => {
+    
+  },
+  computed: {
+    filteredGames() {
+       // filtros aqui
+       return this.games.filter((game) => {
           const statusMatch =
             this.filterStatus === 'all' || game.status === this.filterStatus;
           const dateMatch =
@@ -59,63 +74,18 @@
           return statusMatch && dateMatch && searchMatch;
         });
       },
+  },
+  methods: {
+      //aqui llamaremos a la api y para conseguir los datos de los juegos
+
+    viewRecord(gameId) {
+      //Aqui hay que hacer una llamada a la base de datos para obtener los juegos del jugador en base al ID y cambiar de vista
+      console.log(`View Record for Game ID: ${gameId}`);
     },
-    methods: {
-      // Fetch the list of games from your API or data source here
-      // Fetch game details based on game ID and set selectedGame
-      // Implement game emulation based on logs
-      // You can use methods to perform these actions
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .game-finder {
-    font-family: Arial, sans-serif;
-    padding: 20px;
-  }
-  
-  .filters {
-    margin-bottom: 20px;
-  }
-  
-  .filters label {
-    margin-right: 10px;
-  }
-  
-  .game-list {
-    list-style: none;
-    padding: 0;
-  }
-  
-  .game-item {
-    display: flex;
-    align-items: center;
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
-  
-  .game-name {
-    flex: 1;
-  }
-  
-  .view-details-button {
-    background-color: #354a5e;
-    color: #fff;
-    border: none;
-    padding: 5px 10px;
-    cursor: pointer;
-  }
-  
-  .view-details-button:hover {
-    background-color: #2e3e4f;
-  }
-  
-  .game-details {
-    margin-top: 20px;
-    padding: 10px;
-    border: 1px solid #ccc;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+/* ... your existing styles ... */
+</style>

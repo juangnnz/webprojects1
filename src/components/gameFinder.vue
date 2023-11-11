197681780eac03ed4ccb4095277e353a286a65c2
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-11-11 14:31:58 +0100

    adding examples to see if search game will work

--- src/components/gameFinder.vue
+++ src/components/gameFinder.vue
@@ -48,43 +48,122 @@
   </div>
 </template>
 
+<template>
+  <div class="game-finder">
+    <h2 id="searchGamesTitle">Search Games</h2>
+    <div class="filters">
+      <select v-model="filterStatus">
+        <option value="all">All</option>
+        <option value="finished">Finished</option>
+        <option value="available">Available</option>
+      </select>
+
+      <label>Between Dates:</label>
+      <input type="date" v-model="startDate" />
+      <input type="date" v-model="endDate" />
+
+      <div class="search-container">
+        <input v-model="searchQuery" type="text" class="search-input" placeholder="Search...">
+      </div>
+    </div>
+
+    <ul class="game-list">
+      <li v-for="game in filteredGames" :key="game.id" class="game-item">
+        <span class="game-name">{{ game.name }}</span>
+        <button class="view-details-button" @click="viewGameDetails(game.id)">View Details</button>
+      </li>
+    </ul>
+
+    <div v-if="selectedGame" class="game-details">
+      <h3>Game Details</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Game ID</th>
+            <th>Size</th>
+            <th>HP_max</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ selectedGame.id }}</td>
+            <td>{{ selectedGame.size }}</td>
+            <td>{{ selectedGame.HP_max }}</td>
+            <td><button @click="viewRecord(selectedGame.id)">View Record</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      games: [], // aqui cogeremos todo de la API
-        filterStatus: 'all',
-        startDate: '',
-        endDate: '',
-        searchQuery: '',
-        selectedGame: null,
-      };
-    
+      games: [
+        {
+          id: 1,
+          name: 'Game 1',
+          status: 'finished',
+          startDate: '2023-01-01',
+          endDate: '2023-01-10',
+          size: 'Large',
+          HP_max: 100,
+        },
+        {
+          id: 2,
+          name: 'Game 2',
+          status: 'available',
+          startDate: '2023-02-15',
+          endDate: '2023-02-28',
+          size: 'Medium',
+          HP_max: 150,
+        },
+        {
+          id: 3,
+          name: 'Game 3',
+          status: 'available',
+          startDate: '2023-03-01',
+          endDate: '2023-03-15',
+          size: 'Small',
+          HP_max: 80,
+        },
+      ],
+      filterStatus: 'all',
+      startDate: '',
+      endDate: '',
+      searchQuery: '',
+      selectedGame: null,
+    };
   },
   computed: {
     filteredGames() {
-       // filtros aqui
-       return this.games.filter((game) => {
-          const statusMatch =
-            this.filterStatus === 'all' || game.status === this.filterStatus;
-          const dateMatch =
-            (!this.startDate || game.startDate >= this.startDate) &&
-            (!this.endDate || game.endDate <= this.endDate);
-          const searchMatch = game.name.toLowerCase().includes(this.searchQuery.toLowerCase());
-          return statusMatch && dateMatch && searchMatch;
-        });
-      },
+      return this.games.filter((game) => {
+        const statusMatch =
+          this.filterStatus === 'all' || game.status === this.filterStatus;
+        const dateMatch =
+          (!this.startDate || game.startDate >= this.startDate) &&
+          (!this.endDate || game.endDate <= this.endDate);
+        const searchMatch = game.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+        return statusMatch && dateMatch && searchMatch;
+      });
+    },
   },
   methods: {
-      //aqui llamaremos a la api y para conseguir los datos de los juegos
-
     viewRecord(gameId) {
-      //Aqui hay que hacer una llamada a la base de datos para obtener los juegos del jugador en base al ID y cambiar de vista
       console.log(`View Record for Game ID: ${gameId}`);
+      //aqui vemos el record del juego
+    },
+    viewGameDetails(gameId) {
+      // aqui se ven los detalles del juego
+      this.selectedGame = this.games.find((game) => game.id === gameId);
     },
   },
 };
 </script>
+
 
 
 <style scoped>

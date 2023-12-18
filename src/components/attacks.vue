7a7f918e5a530e556da9e1f562271e5d257bfe9e
Juan González Díaz <j.gonzalezdiaz@students.salle.url.edu>
2023-12-18 14:23:04 +0100

    implementation of the equipped atacks of a user

--- src/components/attacks.vue
+++ src/components/attacks.vue
@@ -1,117 +1,97 @@
 <template>
-    <main class="attacks-management">
-      <h2 class="title">Manage Equipped Attacks</h2>
-  
-      <section class="equipped-attacks">
-        <h3>Equipped Attacks</h3>
-        <ul>
-          <li v-for="attack in equippedAttacks" :key="attack.id" class="equipped-attack">
-            <h4>{{ attack.name }}</h4>
-            <p class="attack-description">{{ attack.description }}</p>
-            <button @click="unequipAttack(attack)">Unequip</button>
-          </li>
-        </ul>
-      </section>
-  
-      <section class="available-attacks">
-        <h3>Available Attacks</h3>
-        <ul>
-          <li v-for="attack in availableAttacks" :key="attack.id" class="available-attack">
-            <h4>{{ attack.name }}</h4>
-            <p class="attack-description">{{ attack.description }}</p>
-            <button @click="equipAttack(attack)">Equip</button>
-          </li>
-        </ul>
-      </section>
-    </main>
-  </template>
+  <main class="attacks-management">
+    <h2 class="title">Manage Equipped Attacks</h2>
 
+    <section class="equipped-attacks">
+      <h3>Equipped Attacks</h3>
+      <ul>
+        <li v-for="attack in equippedAttacks" :key="attack.id" class="equipped-attack">
+          <h4>{{ attack.name }}</h4>
+          <p class="attack-description">{{ attack.description }}</p>
+          <button @click="unequipAttack(attack)">Unequip</button>
+        </li>
+      </ul>
+    </section>
+
+    <section class="available-attacks">
+      <h3>Available Attacks</h3>
+      <ul>
+        <li v-for="attack in availableAttacks" :key="attack.id" class="available-attack">
+          <h4>{{ attack.name }}</h4>
+          <p class="attack-description">{{ attack.description }}</p>
+          <button @click="equipAttack(attack)">Equip</button>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
 
 <script>
 export default {
   data() {
     return {
-      equippedAttacks: [
-        { id: 1, name: "Attack 1" },
-        { id: 2, name: "Attack 2" },
-        { id: 3, name: "Attack 3" },
-      ],
-      availableAttacks: [
-        { id: 4, name: "New Attack 1" },
-        { id: 5, name: "New Attack 2" },
-        { id: 6, name: "New Attack 3" },
-      ],
+      equippedAttacks: [],
+      availableAttacks: [],
     };
   },
+  mounted() {
+    // Fetch attacks when the component is mounted
+    this.fetchAttacks();
+  },
   methods: {
-    equipAttack(attack) {
-      if (this.equippedAttacks.length < 3) {
-        this.equippedAttacks.push(attack);
-        this.availableAttacks = this.availableAttacks.filter((a) => a.id !== attack.id);
+    async fetchAttacks() {
+      try {
+        const token = this.$route.query.token;
+        const playerId = this.$route.query.player_ID;
+        const response = await fetch(`https://your-api-url/players/${playerId}/attacks`);
+        const data = await response.json();
+
+        this.equippedAttacks = data.equippedAttacks;
+        this.availableAttacks = data.availableAttacks;
+      } catch (error) {
+        console.error('Error fetching attacks:', error);
       }
     },
-    unequipAttack(attack) {
-      this.availableAttacks.push(attack);
-      this.equippedAttacks = this.equippedAttacks.filter((a) => a.id !== attack.id);
+    async equipAttack(attack) {
+      if (this.equippedAttacks.length < 3) {
+        try {
+          const response = await fetch(`https://your-api-url/players/attacks/${attack.id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ equipped: true }),
+          });
+
+          // Assuming the API responds with the updated list of equipped and available attacks
+          const data = await response.json();
+
+          this.equippedAttacks = data.equippedAttacks;
+          this.availableAttacks = data.availableAttacks;
+        } catch (error) {
+          console.error('Error equipping attack:', error);
+        }
+      }
+    },
+    async unequipAttack(attack) {
+      try {
+        const response = await fetch(`https://your-api-url/players/attacks/${attack.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ equipped: false }),
+        });
+
+        // Assuming the API responds with the updated list of equipped and available attacks
+        const data = await response.json();
+
+        this.equippedAttacks = data.equippedAttacks;
+        this.availableAttacks = data.availableAttacks;
+      } catch (error) {
+        console.error('Error unequipping attack:', error);
+      }
     },
   },
 };
 </script>
-
-<style scoped>
-.attacks-management {
-  text-align: center;
-  padding: 20px;
-}
-
-.equipped-attacks {
-  margin-top: 20px;
-}
-
-.equipped-attack {
-  background-color: #DEEBFF;
-  border: none;
-  border-radius: 5px; 
-  margin: 10px;
-  padding: 10px;
-  display: inline-block;
-}
-
-.available-attacks {
-  margin-top: 20px;
-}
-
-.available-attack {
-  background-color: #DEEBFF;
-  border: none;
-  border-radius: 5px; 
-  margin: 10px;
-  padding: 10px;
-  display: inline-block;
-}
-
-button {
-  background-color: #3D5CFF;
-  color: #ffffff;
-  border: none;
-  padding: 5px 10px;
-  cursor: pointer;
-  border-radius: 5px;
-}
-
-button:hover {
-  background-color: #0056b3;
-}
-
-.title {
-  color: #3D5CFF;
-}
-
-h3{
-  color: black;
-}
-
-h4{
-  color: black;
-}
-</style>
